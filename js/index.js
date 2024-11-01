95bc6f9a4df4a9b35f8ee7f3f5f2471f4d3e1979
Sofiara <sofia1964@gmail.ru>
2024-11-01 15:21:15 +0300

    первый коммит

---
+++ js/index.js
@@ -0,0 +1,11 @@
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-lower__list');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header-lower__list_active');
+    })
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header-lower__list_active');
+    });
+}())
